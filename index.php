1001ca73c5fa8a5cab9dc78d167fca99520c0f0b
MADHHMATISAVOOR <70912139+MADHHMATISAVOOR@users.noreply.github.com>
2020-09-10 11:27:51 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<header>
+		<title>Education For Girl Child</title>
+		<link rel="stylesheet" href="style.css" type="text/CSS"/>
+	</header>
+	<body>
+		
+		<div class="container">
+			<h1>Welcome To Education For Girl Child</h1>
+			<p>The amount you donate will be used for funding children in the backward areas.</p>
+		</div>
+		<div class="container">
+			<p>Click the donate button to head to the payments page.</p>
+			<center><a href="form.php"><button type="button">Donate</button></a></center>
+		</div>
+		<footer>
+			<div>
+				<marquee width="100%" direction="left" height="30%">
+					Copyright © Education For Girl Child | For any feedback call us at +91000000000
+				</marquee>
+			</div>
+		</footer>
+
+	</body>
+</html>
