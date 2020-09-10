feb6f86914216666e613aec2c103ecdb3849a460
MADHHMATISAVOOR <70912139+MADHHMATISAVOOR@users.noreply.github.com>
2020-09-10 11:25:22 +0530

    cancel.php

---
+++ cancle.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>
+			Education For Girl Child
+		</title>
+		<link rel="stylesheet" href="style.css" type="text/CSS">
+	</head>
+	<body>
+		<div class="container">
+		<h1>Transaction Unsuccessful</h1>
+		<center><img style="width:20%; height:40%" src="images/emoji.png" alt="sad"></center>
+		</div>
+		<div class="container">
+			<center><a href="index.php"><button style="width:150px" type="button">Back to home</button></a></center>
+		</div>
+		<footer>
+			<div>
+				<marquee width="100%" direction="left" height="30%">
+					Copyright © Education For Girl Child | For any feedback call us at +91000000000
+				</marquee>
+			</div>
+		</footer>
+	</body>
+</html>
